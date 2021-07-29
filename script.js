1c71fcaf8bfbe7b4af1611fa5246357b2534a2f9
Grejo Joby <grejo00@gmail.com>
2021-07-29 13:50:15 +0530

    form

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-(function () {
-    'use strict'
-    const forms = document.querySelectorAll('.requires-validation')
-    Array.from(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
+// (function () {
+//     'use strict'
+//     const forms = document.querySelectorAll('.requires-validation')
+//     Array.from(forms)
+//         .forEach(function (form) {
+//             form.addEventListener('submit', function (event) {
+//                 if (!form.checkValidity()) {
+//                     event.preventDefault()
+//                     event.stopPropagation()
+//                 }
+
+//                 form.classList.add('was-validated')
+//             }, false)
+//         })
+// })()
+
+$(function()
+ {
+    $("input").on("change keyup",calculate)
+})
+
+var theForm = document.forms["cgpaform"];
+
+function calculate() {
+    var total = $("sem1").val() + $("sem2").val() +$("sem3").val() +$("sem4").val() +$("sem5").val() +$("sem6").val() +$("sem7").val() +$("sem8").val(); 
+    var aggregate = total/8;
+    var percent = 0;
+    document.getElementById('agg').value = aggregate;
+    // $('#agg').val() = aggregate;
+    if(aggregate>=7){
+        percent = (7.4*aggregate) + 12;
+    }
+    else if(aggregate<7 && aggregate>0){
+        percent = (7.1*aggregate) + 12;
+    }
+    $('#perc').val() = percent;
     
-          form.classList.add('was-validated')
-        }, false)
-      })
-    })()
-    
+}
